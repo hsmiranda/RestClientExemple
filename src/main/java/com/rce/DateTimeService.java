f5ef3cda1533007b08b485a5c8112353922c1a68
Herberson Miranda <553757+hsmiranda@users.noreply.github.com>
2023-10-02 22:17:26 -0300

    Feat: Implemented circuit breaker condition.

--- src/main/java/com/rce/DateTimeService.java
+++ src/main/java/com/rce/DateTimeService.java
@@ -3,6 +3,7 @@ package com.rce;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
@@ -12,6 +13,12 @@ public interface DateTimeService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Fallback(fallbackMethod = "fallback")
+    @CircuitBreaker(
+        requestVolumeThreshold=4,
+        failureRatio=0.5,
+        delay = 2000,
+        successThreshold=2
+    )
     public Now getNow();
 
     default Now fallback(){
